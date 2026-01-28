f204721574736277dd284f99c2bba0980fe7a23f
dltnghks <dltrnghks@naver.com>
2026-01-28 10:25:16 +0900

    feat : 노션 카테고리 추가

--- sync_notion.js
+++ sync_notion.js
@@ -30,9 +30,11 @@ async function fetchNotionPosts() {
   for (const page of response.results) {
     const props = page.properties;
     
+    // 노션 태그랑 일치해야 됨.
     const title = props.Name?.title?.[0]?.plain_text || "Untitled";
     const date = props.Date?.date?.start || new Date().toISOString().split('T')[0];
     const tags = props.Tag?.multi_select?.map(t => t.name) || [];
+    const categories = props.Categories?.multi_select?.map(t => t.name) || [];
     
     const safeTitle = title.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9가-힣\-_]/g, '');
     const fileName = `${date}-${safeTitle}.md`;
@@ -44,24 +46,7 @@ async function fetchNotionPosts() {
     const mdString = n2m.toMarkdownString(mdblocks);
     const content = mdString.parent || "";
 
-    // --- 핵심 수정: 본문 내용을 description으로 변환 ---
-    // 마크다운 문법 제거 후 순수 텍스트만 추출
-    let plainText = content
-      .replace(/^#+\s+/gm, '') // 헤더 제거
-      .replace(/(\*\*|__)(.*?)\1/g, '$2') // 볼드 제거
-      .replace(/(\*|_)(.*?)\1/g, '$2') // 이탤릭 제거
-      .replace(/!\[.*?\]\(.*?\)/g, '') // 이미지 태그 제거
-      .replace(/\^([^\]]+)\]\(.*?\)/g, '$1') // 링크 텍스트만 남김
-      .replace(/\n/g, ' ') // 줄바꿈 -> 공백
-      .replace(/\s+/g, ' ') // 다중 공백 -> 하나로
-      .trim();
-
-    // 150자 정도로 자르기 (미리보기에 적당한 길이)
-    const description = plainText.length > 150 
-      ? plainText.substring(0, 150) + "..." 
-      : plainText;
-
-    const frontmatter = `---\nlayout: post\ntitle: "${title}"\ndate: ${date} 00:00:00 +0900\ncategories: [Notion]\ntags: [${tags.join(", ")}]\ndescription: "${description.replace(/"/g, '\\"')}"\n---`
+    const frontmatter = `---\nlayout: post\ntitle: "${title}"\ndate: ${date} 00:00:00 +0900\ncategories: [${categories.join(", ")}]\ntags: [${tags.join(", ")}]\n---\n\n`
 
     fs.writeFileSync(filePath, frontmatter + content, 'utf8');
     console.log(`Synced: ${fileName} (Description added)`);
